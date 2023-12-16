4ba2d8b9a9a752822890932b74459cad80d5a183
Dmitrii <egev604@mail.ru>
2023-12-16 19:18:30 +0300

    Remove service

--- service/first-nest-project/src/dto/user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, IsInt } from 'class-validator';
-
-export class UserDto {
-    @IsString()
-    login: string;
-    @IsString()
-    password: string;
-}
--- service/first-nest-project/src/models/passage/passage.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PassageService } from './passage.service';
-import { PassageController } from './passage.controller';
-
-@Module({
-  providers: [PassageService],
-  controllers: [PassageController]
-})
-export class PassageModule {}
