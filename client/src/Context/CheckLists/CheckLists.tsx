11dc42281f1376d61a04b6027a501fbd8b487977
Dmitrii <egev604@mail.ru>
2023-12-16 17:55:56 +0300

    checkerror

--- client/src/Context/CheckLists/CheckLists.tsx
+++ client/src/Context/CheckLists/CheckLists.tsx
@@ -1,55 +1,10 @@
 import React from 'react';
 import {List, ListSubheader} from "@mui/material";
-import ListItem from "./ListItem/ListItem";
+import ListItem from "./tests/ListItem/ListItem";
 function CheckLists() {
-    const exampleArray = [
-        {
-            text: 'Элемент 1',
-            passed: false, // Добавьте это свойство для управления состоянием флажка
-            children: [
-                {
-                    text: 'Подэлемент 1.1',
-                    passed: false,
-                    children: [
-                        { text: 'Подподэлемент 1.1.1', passed: false },
-                        { text: 'Подподэлемент 1.1.2', passed: false },
-                    ],
-                },
-                {
-                    text: 'Подэлемент 1.2',
-                    passed: false,
-                    children: [{ text: 'Подподэлемент 1.2.1', passed: false }],
-                },
-            ],
-        },
-        {
-            text: 'Элемент 2',
-            passed: false,
-            children: [
-                {
-                    text: 'Подэлемент 2.1',
-                    passed: false,
-                    children: [{ text: 'Подподэлемент 2.1.1', passed: false }],
-                },
-                { text: 'Подэлемент 2.2', passed: false },
-            ],
-        },
-    ];
+
     return (
-        <List
-            sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-            component="nav"
-            aria-labelledby="nested-list-subheader"
-            subheader={
-                <ListSubheader component="div" id="nested-list-subheader">
-                    Nested List Items
-                </ListSubheader>
-            }
-        >
-            {exampleArray.map((item, index) => (
-                <ListItem key={index} item={item} />
-            ))}
-        </List>
+        <></>
     );
 }
 
