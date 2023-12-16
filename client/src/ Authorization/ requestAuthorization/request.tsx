1763d911cdcfa8484e86a120329af64f2d9ea1b8
Dmitrii <egev604@mail.ru>
2023-12-16 16:07:44 +0300

    bemsik

--- client/src/ Authorization/ requestAuthorization/request.tsx
+++ client/src/ Authorization/ requestAuthorization/request.tsx
@@ -1,13 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-const Request = (login:string, passsword:string) => {
+const Request = (login:string, password:string) => {
     const [token, setToken] = useState<string>("");
 
     useEffect(() => {
         const apiUrl = '';
-        axios.get(apiUrl).then((resp) => {
-            const allPersons = resp.data;
-            setToken(allPersons);
+        axios.post(apiUrl, {auth: {
+                username: login,
+                password: password
+            }})
+            .then((resp) => {
+                const allPersons = resp.data;
+                setToken(allPersons);
         });
     }, [setToken]);
     return token;
